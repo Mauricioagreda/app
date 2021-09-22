640e223bd2c006ae89d1fa77979edd80dffa6058
Mauricioagreda <mauricio.agreda.d2@gmail.com>
2021-09-22 11:20:14 -0300

    app index.js

--- app.js
+++ app.js
@@ -4,6 +4,11 @@ const app = express()
 const port = process.env.PORT || 3000
 
 app.use('/alumno', require('./endpoint/alumno.endpoint'))
+app.use('/', require('./endpoint/index.endpoint'))
+
+app.set('views', './views')
+app.set('view engine', 'ejs')
+    
 
 app.listen(port, () => {
     console.log(`app zunnig on ${port}`)
